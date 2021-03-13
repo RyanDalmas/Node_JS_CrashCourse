b8effbd223db59992f6fae2ce17f5ff2fc7c3ab7
RyanDalmas <rdalm001@ucr.edu>
2021-03-12 18:14:33 -0800

    Finished Crash Course

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ logger.log('Hello there'); */
 const http = require('http');
 const path = require('path');
 const fs = require('fs');
+const { inherits } = require('util');
+const { create } = require('domain');
+const { isMaster } = require('cluster');
 
 const server = http.createServer((req,res) => {
     /* if(req.url == '/') {
@@ -99,3 +102,15 @@ const server = http.createServer((req,res) => {
 const PORT = process.env.PORT || 5000; //Checks if process.env.PORT exists, if not, runs on 5000
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+//Deployment to Heroku
+
+/* git init 
+git add .
+git commit -m "Initial commit"
+heroku create //creates new app
+heroku git:remote -a arcane-woodland-61463
+git push heroku master 
+heroku open */
+
+
